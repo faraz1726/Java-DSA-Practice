27eded2319455a4e1fe0da7a3b603393f9c43802
Faraz <farazahmedq15@gmail.com>
2026-06-28 15:56:52 +0530

    changes made

--- 22 - Array list/sorting.java
+++ 22 - Array list/sorting.java
@@ -7,6 +7,12 @@ public class sorting {
         list.add(9);
         list.add(3);
         list.add(6);
+
+        System.out.println(list);
+        Collections.sort(list);/*in ascending order */
+        System.out.println("after sorting " + list);
+        Collections.sort(list , Collections.reverseOrder());/*in descending or reverse order */
+        System.out.println("in descending order" + list);
     }
     
 }
