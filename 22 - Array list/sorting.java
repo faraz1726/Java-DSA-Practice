fb44594803a1d1f3d12ec03673b6a742bb7a7b5a
Faraz <farazahmedq15@gmail.com>
2026-06-28 15:48:39 +0530

    changes made

---
+++ 22 - Array list/sorting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sorting {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+    }
+    
+}
