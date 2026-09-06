e116f1aeed1b2d8703c58cbd8d9c654ef99311f4
Faraz <farazahmedq15@gmail.com>
2026-09-07 00:14:22 +0530

    new filess added

--- 24 - Stacks P1/ReverseStack.java
+++ 24 - Stacks P1/ReverseStack.java
@@ -7,16 +7,34 @@ public class ReverseStack {
             m.push(s.pop());
             System.out.println(m.peek());
         }
-        
-       
+        /*in this s becomes empty and m has elements  */
     }
+
+
+    public static void reversestack1(Stack<Integer> s){
+        Stack<Integer> m = new Stack<>();
+        while(!s.isEmpty()){
+            m.push(s.pop());
+        }
+
+        while(!m.isEmpty()){
+            s.push(m.pop());
+        }  
+    }
+    /*in this the original stack s is not empty now */
+
+
     public static void main(String args[]){
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
-        reversestack(s);
+
+        reversestack1(s);
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
     
 
     }
