1bd1b1fad35821c56248a370ea6b91d48dc3678d
Faraz <farazahmedq15@gmail.com>
2026-09-11 01:30:07 +0530

    new files added

---
+++ 26 - Queues/deque1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.LinkedList;
+public class deque1 {
+    public static void main(String args[]){
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        System.out.println(deque);
+        deque.addLast(3);
+        deque.addLast(4);
+        System.out.println(deque);
+        deque.removeFirst();
+        System.out.println(deque);
+        deque.removeLast();
+        System.out.println(deque);
+        System.out.println("first element is " + deque.getFirst());
+        System.out.println("last element is " + deque.getLast());
+
+    }
+}
