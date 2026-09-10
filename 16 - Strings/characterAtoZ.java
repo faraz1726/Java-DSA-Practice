3449e7154330f7e0b5b08dff00ee9b411be4cbb4
Faraz <farazahmedq15@gmail.com>
2026-09-10 23:58:42 +0530

    new files added

---
+++ 16 - Strings/characterAtoZ.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class characterAtoZ {
+    public static void main(String args[]){
+        int count = 0;
+        int ch = 'a';
+        char x = 'a';
+        while(count<26){
+            System.out.print(x+" is ");
+            System.out.println(ch);
+            ch++;
+            count++;
+            x++;
+        }
+    }
+    
+}
