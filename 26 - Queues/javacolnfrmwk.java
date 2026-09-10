4c61dc9e6e6b62666d49856b2cadbf8c9e916ac9
Faraz <farazahmedq15@gmail.com>
2026-09-10 13:58:13 +0530

    new filess added

---
+++ 26 - Queues/javacolnfrmwk.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.LinkedList;
+/*Note :- Queue is a framework , not a class , so we cannot make objects of queue.
+          so for that purpose there are 2 classes that can implement queue i.e
+          LinkedList and Arraydeque*/
+public class javacolnfrmwk {
+    public static void main(String args[]){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+    
+}
