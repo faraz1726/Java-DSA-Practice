ce4d934e43a08031593754d6130641d544115dd1
Faraz <farazahmedq15@gmail.com>
2026-09-10 13:59:10 +0530

    new filess added

--- 26 - Queues/javacolnfrmwk.java
+++ 26 - Queues/javacolnfrmwk.java
@@ -5,7 +5,8 @@ import java.util.LinkedList;
           LinkedList and Arraydeque*/
 public class javacolnfrmwk {
     public static void main(String args[]){
-        Queue<Integer> q = new LinkedList<>();
+        //Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
