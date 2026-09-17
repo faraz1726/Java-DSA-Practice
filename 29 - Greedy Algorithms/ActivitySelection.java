3dd20ec7d652076705b04a48601e464df8a6463e
Faraz <farazahmedq15@gmail.com>
2026-09-17 23:56:10 +0530

    new files added

--- 29 - Greedy Algorithms/ActivitySelection.java
+++ 29 - Greedy Algorithms/ActivitySelection.java
@@ -4,20 +4,31 @@ public class ActivitySelection {
     public static void main(String args[]){
         int[] start = {1,3,0,5,8,5};
         int[] end = {2,4,6,7,9,9};
+        
+        //making a 2d array , and storing index , start time , end time 
+        int activities[][] = new int[start.length][3];
+        for(int i = 0 ; i<start.length ; i++){
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        // sorting 2d array in java , on the basis of coloum 2 which is end time
+        Arrays.sort(activities , Comparator.comparingDouble(o -> o[2]));
 
-        //sorting on end time
+
+        //already sorted on the basis of end time
         int maxmactivity = 0;
         ArrayList<Integer> ans = new ArrayList<>();
 
         //for activity 1 (always selected)
         maxmactivity = 1;
-        ans.add(0);// added ectivity first
-        int lastend = end[0];// end time of first activity
+        ans.add(activities[0][0]);// added activity first
+        int lastend = activities[0][2];// end time of first activity
         for( int i = 0 ; i<end.length ; i++){
-            if(start[i] >= lastend){
+            if(activities[i][1] >= lastend){
                  maxmactivity++;
-                 ans.add(i);
-                 lastend = end[i];
+                 ans.add(activities[i][0]);
+                 lastend = activities[i][2];
             }
         }
         System.out.println("maximum activity a person can perform is "+maxmactivity);
